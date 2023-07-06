c52677f3237b0ae022f04cc2fadccb326dfef635
MuriloFuza <muriloacademix@gmail.com>
2023-07-06 17:53:00 -0400

    Iniciando a criação da página de gastos com cartão de crédito

---
+++ src/app/card/page.tsx
@@ -0,0 +1,9 @@
+export default function CreditCard() {
+  return (
+    <div className="space-y-4">
+      <h1 className="capitalize font-bold text-xl">
+        Gastos com cartão de crédito
+      </h1>
+    </div>
+  )
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,15 @@
 'use client'
 import Link from 'next/link'
-import { Home, Landmark, PieChart } from 'lucide-react'
+import { Home, Landmark, PieChart, CreditCard } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import { UserButton } from '@clerk/nextjs'
 
 export function Header() {
   const pathname = usePathname()
 
+  const isAtHome = pathname === '/'
   const isAtHistory = pathname === '/history'
+  const isAtCard = pathname === '/card'
 
   return (
     <div className="flex justify-between items-center p-3">
@@ -19,7 +21,7 @@ export function Header() {
       <nav className="flex gap-x-2">
         <Link
           className={`p-2 border-y-2 border-transparent text-neutral-400 hover:text-neutral-200 ${
-            !isAtHistory ? 'border-b-blue-500 text-neutral-50' : ''
+            isAtHome ? 'border-b-blue-500 text-neutral-50' : ''
           }`}
           href="/"
           title="Inicio"
@@ -34,6 +36,15 @@ export function Header() {
           title="Histórico"
         >
           <PieChart size={24} />
+        </Link>
+        <Link
+          className={`p-2 border-y-2 border-transparent text-neutral-400 hover:text-neutral-200 ${
+            isAtCard ? 'border-b-blue-500 text-neutral-50' : ''
+          }`}
+          href="/card"
+          title="Crédito"
+        >
+          <CreditCard size={24} />
         </Link>
       </nav>
     </div>
